a99fd9bd86ccb4c36579531a3abb4b65c00968c9
Anton Hibl <75855379+antonhibl@users.noreply.github.com>
2021-11-23 15:27:41 -0700

    linked byte generator

--- link.h
+++ link.h
@@ -7,7 +7,7 @@
 #include <stdbool.h>
 
 // Macro Definitions
-#define BYTE_SIZE 8
+#define BYTE_SIZE 16
 #define BOOL_HIGH 1
 #define BOOL_LOW 0
 
@@ -20,73 +20,73 @@ struct node{
 
 // Function Implementations
 /*
-Name: 
-Process: 
-Method Input: 
-Method Output(parameters): 
-Method Output(returned): 
-Device Input: 
-Device Output: 
-Dependencies: 
+	Name: getRandBool()
+	Process: generates a pseudo-random boolean value
+	Method Input: none
+	Method Output(parameters): none
+	Method Output(returned): random boolean value
+	Device Input: none
+	Device Output: none
+	Dependencies: rand()
 */
 int getRandBool();
 
 /*
-Name: 
-Process: 
-Method Input: 
-Method Output(parameters): 
-Method Output(returned): 
-Device Input: 
-Device Output: 
-Dependencies: 
+	Name: generateByte
+	Process: generates an empty space in memory to hold our byte
+	Method Input: none
+	Method Output(parameters): none
+	Method Output(returned): pointer to reserved empty space for a byte 
+	Device Input: none
+	Device Output: none
+	Dependencies: malloc(), sizeof() 
 */
 struct node *generateByte(struct node *headNode);
 
 /*
-Name: 
-Process: 
-Method Input: 
-Method Output(parameters): 
-Method Output(returned): 
-Device Input: 
-Device Output: 
-Dependencies: 
+	Name: mungeByte 
+	Process: fills a byte bit-by-bit randomly with 0s and 1s
+	Method Input: base index of the empty byte
+	Method Output(parameters): none
+	Method Output(returned): pointer to the base index of the filled byte
+	Device Input: none
+	Device Output: none
+	Dependencies: getRandBool()
 */
 struct node *mungeByte(struct node *headNode);
 
 /*
-Name: 
-Process: 
-Method Input: 
-Method Output(parameters): 
-Method Output(returned): 
-Device Input: 
-Device Output: 
-Dependencies: 
+	Name: printByte
+	Process: prints and displays a byte with it's corresponding data
+	Method Input: base index of the given byte
+	Method Output(parameters): none
+	Method Output(returned): none
+	Device Input: none
+	Device Output: Prints and displays a byte with corresponding data
+	Dependencies: printf(), bitToDecimal
 */
 void printByte(struct node *headNode);
 
 /*
-Name: 
-Process: 
-Method Input: 
-Method Output(parameters): 
-Method Output(returned): 
-Device Input: 
-Device Output: 
-Dependencies: 
+	Name: clearByte
+	Process: clears a given byte bit-by-bit
+	Method Input: base index of the given byte
+	Method Output(parameters): none
+	Method Output(returned): boolean success flag
+	Device Input: none
+	Device Output: none
+	Dependencies: free()
 */
 bool clearByte(struct node *headNode);
 
 /*
-Name: 
-Process: 
-Method Input: 
-Method Output(parameters): 
-Method Output(returned): 
-Device Input: 
-Device Output: 
-Dependencies: 
+	Name: bitToDecimal
+	Process: converts a boolean value from a bit into it's decimal value
+	Method Input: the bit value and the Nth ordination(little-endian index)
+	Method Output(parameters): none
+	Method Output(returned): decimal value of the bit inside of the byte
+	Device Input: none
+	Device Output: none
+	Dependencies: powf() 
 */
-long byteToDecimal(struct node *headNode);
+int bitToDecimal(int node_data, float N);
