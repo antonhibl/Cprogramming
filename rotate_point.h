70f7791b74ad4bb70a30cb2ae6251ee1d8958d94
Anton Hibl <75855379+antonhibl@users.noreply.github.com>
2021-11-23 20:55:39 -0700

    Point Rotation Calculator

---
+++ rotate_point.h
@@ -0,0 +1,24 @@
+#pragma once
+
+// Macro Definitions
+#define PI 3.14159
+
+// Data Structure Implementation(s)
+struct coords{
+    double x;
+    double y;
+};
+
+// Function Specifications
+
+/*
+    Name: 
+    Process: 
+    Method Input: 
+    Method Output(parameters): 
+    Method Output(returned): 
+    Device Input: 
+    Device Output: 
+    Dependencies: 
+*/
+void rotatePoints(struct coords *inputCoords, double rotation_degrees);
